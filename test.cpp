85c7b855da39a30be627f61cd33f4460228d473e
kkke <kkke@nwsuaf.edu.cn>
2017-03-05 20:14:27 +0800

    changed test.cpp

--- test.cpp
+++ test.cpp
@@ -2,12 +2,9 @@
 
 using namespace std;
 
-long long x1, x2, x3;
-
 int main()
 {
-	cin >> x1 >> x2 >> x3;
-	cout << (max(x1,max(x2,x3)) - min(x1, min(x2, x3))) << endl;
+	cout<<"do something!";
 	return 0;
 }
 
