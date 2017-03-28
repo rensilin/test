60eadd3f9625b997a1ad781162358fa57911ab21
kkke <kkke@nwsuaf.edu.cn>
2017-03-28 20:56:58 +0800

    add math.cpp

---
+++ math.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const double PI=4*atan(1);
+
+int main()
+{
+	printf("%.15lf\n",PI);
+	cout<<"PI="<<PI<<endl;
+	int a;
+	cin>>a;
+	cout<<"sin(a)="<<sin(a)<<endl;
+	cout<<"cos(a)="<<cos(a)<<endl;
+	return 0;
+}
