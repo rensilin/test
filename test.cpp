1d19afc13668c18a22219c9952bd738e53d8a4b3
kkke <kkke@nwsuaf.edu.cn>
2017-05-20 22:47:38 +0800

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,48 @@
+#include <cstdio>
+#include <cstring>
 #include <iostream>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
+class iter:public iterator<std::random_access_iterator_tag,int>{
+private:
+	int n;
+public:
+	iter()
+	{
+	}
+	iter(int i)
+	{
+		n=i;
+	}
+	int operator*()
+	{
+		return n;
+	}
+	friend bool operator!=(const iter &a,const iter &b)
+	{
+		return a.n!=b.n;
+	}
+	void operator+=(int i)
+	{
+		n+=i;
+	}
+	void operator++()
+	{
+		n++;
+	}
+	difference_type operator-(const iter &o)
+	{
+		return n-o.n;
+	}
+};
+
 int main()
 {
-	cout<<"I get the en_GB git"<<endl;
+	iter i=lower_bound(iter(0),iter(11),4);
+	cout<<*i<<endl;
 	return 0;
 }
 
