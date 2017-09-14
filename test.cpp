94510cedda682703fd44734d10db214ef4a23898
kkke <kkke@nwsuaf.edu.cn>
2017-09-14 14:36:59 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,23 +1,35 @@
-//判线段与空间三角形相交,包括交于边界和(部分)包含 int
-intersect_in(line3 l,plane3 s){
-	return !same_side(l.a,l.b,s)&&!same_side(s.a,s.b,l.a,l.b,s.c)&&
-		!same_side(s.b,s.c,l.a,l.b,s.a)&&!same_side(s.c,s.a,l.a,l.b,s.b);
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+template<class TN>
+inline void kread(TN &x)
+{
+    x=0;
+    char c;
+    bool flag=false;
+    while(!isdigit(c=getchar()))
+       if(c=='-')
+            flag=true;
+    do{
+        x=x*10+c-48;
+    }while(isdigit(c=getchar()));
+    if(flag)x=-x;
 }
 
-int intersect_in(point3 l1,point3 l2,point3 s1,point3 s2,point3 s3){
-	return !same_side(l1,l2,s1,s2,s3)&&!same_side(s1,s2,l1,l2,s3)&&
-		!same_side(s2,s3,l1,l2,s1)&&!same_side(s3,s1,l1,l2,s2);
+template<class TN,class... ARGS>
+inline void kread(TN &first,ARGS& ... args)
+{
+    kread(first);
+    kread(args...);
 }
 
-//判线段与空间三角形相交,不包括交于边界和(部分)包含 int
-intersect_ex(line3 l,plane3 s){
-	return
-		opposite_side(l.a,l.b,s)&&opposite_side(s.a,s.b,l.a,l.b,s.c)&&
-		opposite_side(s.b,s.c,l.a,l.b,s.a)&&opposite_side(s.c,s.a,l.a,l.b,s.b);
+using namespace std;
+
+int main()
+{
+	return 0;
 }
 
-int intersect_ex(point3 l1,point3 l2,point3 s1,point3 s2,point3 s3){
-	return
-		opposite_side(l1,l2,s1,s2,s3)&&opposite_side(s1,s2,l1,l2,s3)&&
-		opposite_side(s2,s3,l1,l2,s1)&&opposite_side(s3,s1,l1,l2,s2);
-}
--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-from PIL import Image
-im=Image.open("/media/kkke/文件/photo/IMG_20161013_002437_HDR.jpg")
-print(im.format,im.size,im.mode)
-im.thumbnail((2000,1000))
-im.save('thum.jpg','JPEG')
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Standard scientific Python imports  
+import matplotlib.pyplot as plt  
+  
+# Import datasets, classifiers and performance metrics  
+from sklearn import datasets, svm, metrics  
+  
+# The digits dataset  
+digits = datasets.load_digits() 
+
+image_and_labels = list(zip(digits.images,digits.target))
+for index,(image,label) in enumerate(image_and_labels[:8]):
+    plt.subplot(2,4,index+1)
+    plt.axis('off')
+    plt.imshow(image,cmap=plt.cm.gray_r,interpolation='nearest')
+    plt.title('Training: %i'%label)
+plt.show()
