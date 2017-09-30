85436215eafa97399b4899481c5bc60cc3c54a06
kkke <kkke@nwsuaf.edu.cn>
2017-09-30 21:36:15 +0800

    update

--- test.py
+++ test.py
@@ -11,10 +11,9 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.imshow('origin',img);  
     
 #SIFT  
-detector = cv2.SIFT()  
+detector = cv2.xfeatures2d.SIFT_create()  
 keypoints = detector.detect(gray,None)  
-img = cv2.drawKeypoints(gray,keypoints)  
-#img = cv2.drawKeypoints(gray,keypoints,flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)  
+img = cv2.drawKeypoints(img,keypoints,img,flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)  
 cv2.imshow('test',img);  
 cv2.waitKey(0)  
 cv2.destroyAllWindows()  
